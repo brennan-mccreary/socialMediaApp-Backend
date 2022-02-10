01bab3924d780560b7d9d948c4ca6f0b3a3d4a7c
jasmiaharris <jazharris_31@yahoo.com>
2022-02-09 18:32:57 -0600

    added auth middleware function.

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,6 +5,18 @@ const config = require('config');
 //Auth function
 function auth(req, res, next) {
 
+    const token = req.header('x-auth-token');
+    if (!token) return res.status(401).send('Acess denied. No token provided');
+
+    try{
+        const decoded = jwt.verify(token, config.get('jwtSecret'));
+        req.user = decoded;
+        return next();
+    }catch (ex) {
+        return res.status(400).send('Invalid token.');
+    
+    }
+
 };
 
 //Export
