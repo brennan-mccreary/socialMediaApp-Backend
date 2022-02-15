de41e58e1e7245e7c52ca98fe9ff05d3536a4ba0
jasmiaharris <jazharris_31@yahoo.com>
2022-02-14 18:29:50 -0600

    added post req.

--- routes/posts.js
+++ routes/posts.js
@@ -2,6 +2,7 @@
 const express = require('express'); //Request routing
 const { Post, validatePost } = require('../models/post');
 const auth = require('../middleware/auth'); //JWT presence confirmation middleware
+const { text } = require('express');
 
 //Define router
 const router = express.Router();
@@ -31,7 +32,17 @@ router.put('/like/:id', async (req, res) => {
 ////POST New Post - user _id, text field
 router.post('/post', async (req, res) => {
     try {
+    //      const { error } = validate(req.body);
+    // if ( error )
+    // return res.status(400).send(error);
 
+    const post = new Post({
+        text: req.body.text,
+        ownedBy: req.body.ownedBy
+
+    })
+    await post.save();
+    return res.send(post);
     }
     catch(err) {
         return res.status(500).send(`Internal Server Error: ${err}`);
